99c8bea314ffe6fab5eaf4370bf7f23567f8980c
shukrullojondevnu <shukrullojon.devnu@gmail.com>
2024-01-23 10:48:57 +0500

    deleted unused interface

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MenuItem } from 'primeng/api';
-import { Product } from '../../api/product';
 import { Subscription, debounceTime } from 'rxjs';
 import { LayoutService } from 'src/app/layout/service/app.layout.service';
 
@@ -10,7 +9,7 @@ import { LayoutService } from 'src/app/layout/service/app.layout.service';
 export class DashboardComponent implements OnInit, OnDestroy {
   items!: MenuItem[];
 
-  products!: Product[];
+  products!: any[];
 
   chartData: any;
 
