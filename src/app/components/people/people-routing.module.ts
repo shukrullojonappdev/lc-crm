10738b04a3d4c3c46cc7a6d0fbadefeb991029e9
shukrullojondevnu <shukrullojon.devnu@gmail.com>
2024-01-29 09:36:46 +0500

    added routing

--- src/app/components/people/people-routing.module.ts
+++ src/app/components/people/people-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'students',
+    loadChildren: () =>
+      import('./students/students.module').then((m) => m.StudentsModule)
+  },
+  {
+    path: 'workers',
+    loadChildren: () =>
+      import('./workers/workers.module').then((m) => m.WorkersModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/components/people/workers/workers-routing.module.ts
+++ src/app/components/people/workers/workers-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WorkersComponent } from './workers.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: WorkersComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class WorkersRoutingModule { }
+export class WorkersRoutingModule {}
