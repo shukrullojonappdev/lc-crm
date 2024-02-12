576fc561509a79ffc094b770b5d48e6987f7466c
shukrullojondevnu <shukrullojon.devnu@gmail.com>
2024-02-12 08:31:22 +0500

    added files for student routing

---
+++ src/app/components/student/timetable/timetable.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timetable',
+  templateUrl: './timetable.component.html',
+  styleUrl: './timetable.component.scss'
+})
+export class TimetableComponent {
+
+}
